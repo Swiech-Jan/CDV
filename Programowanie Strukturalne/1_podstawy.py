b1296b7c083c57272f4c2ce4111e0c51cc120a50
Vendox <49166201+Vendox@users.noreply.github.com>
2020-03-29 16:59:25 +0200

    update

--- Programowanie Strukturalne/1_podstawy.py
+++ Programowanie Strukturalne/1_podstawy.py
@@ -1,1 +1,22 @@
 print ("\ntest")
+
+#potęgowanie
+pow=2**10
+print(pow)
+
+text="CDV"
+print(text*2)
+
+#pobieranie danych z klawiatury
+# + konkatenacja
+name=input("Podaj imię:")
+print("Twoje imię: " + name)
+surname=input("Podaj nazwisko:")
+
+lenghtName=str(len(name))
+print("Długość imienia: " + lenghtName)
+
+lenSurname=len(surname)
+print(lenSurname)
+print(type(surname))
+

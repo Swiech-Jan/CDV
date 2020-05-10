36032a8cbd1f90d0d8f4dd658b7f692d7a9959ea
Vendox <49166201+Vendox@users.noreply.github.com>
2020-05-10 18:39:35 +0200

    test

---
+++ Programowanie Strukturalne/2_tablice.py
@@ -0,0 +1,31 @@
+"""
+country=["Poland", "US", "Germany", "GB", "Spain"]
+print(country)
+print("Dodaj do listy dwa kraje")
+#input=country.append
+print("\n1) Wyswietl pierwsze 3 elementy listy")
+print("\n2) Wyswietl elementy listy, ktore dodalem")
+print("\n3) Wyswietl zawartosc listy")
+print("\n4) Wyczysc zawartosc listy")
+print("\n5) Znajdz element w liscie, ktory poda uzytkownik")
+print("\nWybierz opcje z listy")
+
+choice=input("Wprowadz")
+choice=int(choice)
+
+if choice==1:
+    print(country[0:2])
+elif chocie==2:
+    print("2")
+elif chocie==3:
+    print("3")
+elif chocie==4:
+    print("4")
+elif choice==5:
+    print("5")
+""'
+
+
+
+
+
